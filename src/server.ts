f66101c1a62668e8be17227deba5a02b29234b86
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2023-08-19 23:35:23 +0600

    creating db schema,dbmodel,dbinterface

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,24 @@
-import express from 'express';
+
+import app from './app';
 import mongoose from 'mongoose';
-import cors from 'cors';
+
 //const express = require('express')
 //const mongoose = require('mongoose');
 
 
-const app = express()
-const port = 5000
+
+const port:number = 5000
+
+
 //databaseconnection
 async function main() {
     try{
         await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
         console.log('successful connection!')
+
+        app.listen(port, () => {
+          console.log(`Example app listening on port ${port}`)
+        })
     }catch(err){
         console.log(`Failed to connect database`,err)
     }
@@ -20,10 +27,5 @@ async function main() {
    
   }
   main()
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
